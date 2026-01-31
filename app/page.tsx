b181d7839dece677ba554811dfa2bf86c473c4f7
KIM DONG IL <crayonzgrim@gmail.com>
2026-01-31 17:45:18 +0900

    feat: 데스크탑 사이드바 광고 영역 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
 import { ThumbnailEditor } from '@/components/editor/ThumbnailEditor';
+import { KakaoAdFit } from '@/components/ui/KakaoAdFit';
 
 export default function Home() {
   return (
     <div className="h-dvh overflow-hidden bg-zinc-50 dark:bg-black">
-      <ThumbnailEditor />
+      {/* Desktop: Left sidebar ad (160x600) */}
+      <div className="fixed left-4 top-1/2 z-20 hidden -translate-y-1/2 xl:block">
+        <KakaoAdFit
+          unit="DAN-SqUziNjsmYTfdmze"
+          width={160}
+          height={600}
+          className="overflow-hidden shadow-lg"
+        />
+      </div>
+
+      {/* Main content with left margin for desktop ad */}
+      <div className="h-full xl:ml-[176px]">
+        <ThumbnailEditor />
+      </div>
     </div>
   );
 }
